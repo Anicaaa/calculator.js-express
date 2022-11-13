6ccc78f174bbdc35fa6b3023e5ac151500a737f1
Anica Rahman <anicarahman2009@yahoo.it>
2022-11-13 11:56:33 +0000

    bmi calculator done

--- calculator.js
+++ calculator.js
@@ -10,13 +10,25 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   //console.log(req.body.n1);
-
   const num1 = +req.body.n1;
   const num2 = +req.body.n2;
 
   const result = num1 + num2;
 
   res.send(`The result is ${result}`);
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  const weight = parseFloat(req.body.w);
+  const height = parseFloat(req.body.h);
+
+  const resultBMI = weight / (height * height);
+
+  res.send(`Your BMI is ${resultBMI}`);
 });
 
 app.listen(3000, function () {
